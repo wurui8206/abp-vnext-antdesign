a5bcc45f11b0370577d0210e8128528d30daa6d4
wurui8206 <305271898@qq.com>
2024-10-22 16:43:37 +0000

    part 5 route

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet,RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent],
+  imports: [RouterOutlet,HomeComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
